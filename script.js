4e234e250e5b6b769bfcc2f2d25b94d16b0e308b
deepenmhrj <deepenmhrj@gmail.com>
2015-11-04 21:14:51 -0800

    successfully tested (nameOfTrack)

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 $(function() {
+	var $spotifySearch = $('#spotify-search');
 
-    // your code here
+	var $track = $('#track');
 
+	var $results = $('#results');
+
+	var $trackTemplate = _.template($('#track-template').html());
+
+	$spotifySearch.on('submit', function(event){
+		event.preventDefault();		//prevents the url to open
+
+		$results.empty();
+
+		var searchQuery = $track.val();
+
+		var searchUrl = 'https://api.spotify.com/v1/search?type=track&q=' + searchQuery;
+
+		$.get(searchUrl,function(data){
+			var trackResults = data.tracks.items;
+			console.log(trackResults);
+
+			if (trackResults.length > 0){
+				_.each(trackResults, function(result, index){
+					
+					//testing results
+					$results.append($trackTemplate({name:result.name}));
+
+				});
+
+			};
+		});
+	});
 });
